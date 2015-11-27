3a831ae3a72cbf2215d2a3defc0a49c567c71923
Thomas <brandertb@gmail.com>
2015-11-27 17:10:46 +0100

    hitDect [4, 7]

--- NineMenMorris/app/src/main/java/com/example/admin/ninemenmorris/view/BoardView.java
+++ NineMenMorris/app/src/main/java/com/example/admin/ninemenmorris/view/BoardView.java
@@ -40,6 +40,8 @@ public class BoardView extends View {
             int y = (int) event.getY();
             Log.i("coords", "x = " + x + "   y = " + y);
             Log.i("width", "h =" + h + "  h/5 = " + h / 6);
+
+            // Col, row
             // 1,1
             if (x < w / 28 + size && x > w / 28 - size && y < h / 7 + size && y > h / 7 - size) {
                 piecebounds = new Rect(w / 28 - size, h / 7 - size, w / 28 + size, h / 7 + size);
@@ -68,9 +70,13 @@ public class BoardView extends View {
             }else if (x < (w*27) / 28 + size && x > (w*27) / 28 - size && y < h * 6 / 7 + size && y > h * 6 / 7 - size) {
                 piecebounds = new Rect((w*27) / 28 - size, h * 6 / 7 - size, (w*27) / 28 + size, h * 6 / 7 + size);
                 invalidate();
+                // 4, 7
+            }else if (x < w / 2 + size && x > w / 2 - size && y < h * 6 / 7 + size && y > h * 6 / 7 - size) {
+                piecebounds = new Rect(w / 2 - size, h * 6 / 7 - size,  w / 2 + size, h * 6 / 7 + size);
+                invalidate();
             }
 
-
+            // Rect(int left, int top, int right, int bottom)
             return true;
         }
 
